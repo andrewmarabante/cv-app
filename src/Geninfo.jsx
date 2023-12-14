283afd2cbdc7732f4d37b79a2970886a67b0ab16
Andrew Marabante <andrewmarabante@gmail.com>
2023-12-13 18:15:17 -0800

    created components

---
+++ src/Geninfo.jsx
@@ -0,0 +1,6 @@
+function Geninfo()
+{
+    return <h1>GenInfo</h1>
+}
+
+export default Geninfo;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Education from './Education.jsx'
+import Experience from './Experience.jsx'
+import Geninfo from './Geninfo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Education />
+    <Experience />
+    <Geninfo />
   </React.StrictMode>,
 )
